71d81f775b7a224c28674221640e25de7e69ffc2
ViktoriyaVMCG <43714208+ViktoriyaVMCG@users.noreply.github.com>
2021-01-14 11:30:36 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,8 +5,6 @@ locals {
   referer_key = "jJB23u002zBZ14gh50gD"
   primary_domain_name = "www.example.com"
   alternate_domain_name = "example.com"
-  # Pointing  to the lambda arn in this variable here, since tha lambda function can be used by multiple websites.
-  redirect_lambda_arn = "arn:aws:lambda:us-east-1:123456789:function:NonWWWRedirect:1"
   region = "us-east-1"
   cloudfront_ttl = 31536000
   s3_bucket_name = "website-s3-bucket-name"
