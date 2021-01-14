18b17b946b09c42a0563c509177cc73973a7d2d0
ViktoriyaVMCG <43714208+ViktoriyaVMCG@users.noreply.github.com>
2021-01-14 11:03:18 -0500

    Create s3-bucket-policy.tf

---
+++ s3-bucket-policy.tf
@@ -0,0 +1,25 @@
+# The following file is including policy for the S3 bucket
+# And it will allow the public access to the objects in bucket
+
+resource "aws_s3_bucket_policy" "demo_s3_bucket_policy" {
+    depends_on = [aws_s3_bucket_public_access_block.my_bucket_public_access_block]
+    bucket = aws_s3_bucket.demo-s3-bucket.id
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "DEMOBUCKETPOLICY",
+    "Statement": [
+        {
+            "Sid": "PublicRead",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "${aws_s3_bucket.demo-s3-bucket.arn}/*",
+            "Condition": {
+                "StringLike": {"aws:Referer": "${local.referer_key}" }
+            }
+        }
+    ]
+}
+    POLICY
+}
